c0899d96411e80e2d59d19ec079f777756ebd874
brno4758 <brno4758@colorado.edu>
2022-02-10 22:18:59 -0700

    Begin reducing function sizes

--- src/Clerk.java
+++ src/Clerk.java
@@ -122,73 +122,50 @@ public class Clerk extends Employee{
         return sellCustomers;
     }
 
+
+    public void open_store() throws Exception{
+        ArrayList<buyingCustomer> buyCustomers = generateBuyingCustomers();
+        for(buyingCustomer buyer : buyCustomers){ //For each buying customer
+            if(get_store().get_inventory().get_items_of_type(buyer.get_wanted_type()).isEmpty()){ //If there are no items of type that customer wants
+                continue;
+            }
+            attempt_sale(buyer,get_store().get_inventory().get_items_of_type(buyer.get_wanted_type()).get(0)); //Attempt to sell the first item of appropriate type
+        }
+
+        ArrayList<sellingCustomer> sellCustomers = generateSellingCustomers();
+        for(sellingCustomer seller : sellCustomers){
+
+        }
+    }
+
+    private void attempt_sale(buyingCustomer buyer, Item toSellItem){
+
+        if(buyer.haggle_roll(50)){ //If we roll 50% chance and win, sell full price
+            sell_item(toSellItem, toSellItem.get_list_price());
+            System.out.println(get_name() + " sold a " + buyType + " to " + buyCustomers.get(i).get_name() + " for $" + toBuyItem.get_sale_price());
+        }
+        else if(buyer.haggle_roll(75)){ //else if we roll 75% chance and win, sell 90% full price
+            sell_item(toSellItem, toSellItem.get_list_price()*.9);
+            System.out.println(get_name() + " sold a " + buyType + " to " + buyCustomers.get(i).get_name() + " for $" + toBuyItem.get_sale_price() + " after a 10% discount.");
+        }
+        else{
+            System.out.println(get_name() + " tried selling a " + toBuyItem.get_condition().get_condition() + " condition " + toBuyItem.get_new_or_used() + " " + toBuyItem.get_name() + " to " + buyCustomers.get(i).get_name() + " for $" + toBuyItem.get_list_price() + " but customer refused.");
+        }
+    }
+
+    private void sell_item(Item soldItem, double soldPrice){
+        Store s = get_store();
+        s.add_to_sold(soldItem);
+        s.remove_from_inventory(soldItem);
+        s.get_register().set_amount(s.get_register().get_amount() + soldPrice); //Set register amount
+        soldItem.set_day_sold(s.get_calendar().get_current_day()); //Set items sold date to current day
+        soldItem.set_sale_price(soldPrice); //Set items sold price
+    }
+
     // Ran out of time on open_store, but this could be abstracted by creating a buying/selling behavior interface and removing
     // the complicated for loops and if statements to not create excess programming logic levels.
     public void open_store() throws Exception{
         System.out.println(get_name() + " opened the FNMS for business.");
-
-        // Get the inventory, soldItems, cash register of the store to modify
-        Inventory inv = get_store().get_inventory();
-        CashRegister reg = get_store().get_register();
-
-        //Generate buying customers
-        ArrayList<buyingCustomer> buyCustomers = generateBuyingCustomers();
-
-        //Generate selling customers With random item
-        ArrayList<sellingCustomer> sellCustomers = generateSellingCustomers();
-
-//        //buying customer:
-        for (int i = 0; i < buyCustomers.size(); i++) {
-//            //generate a type of item desired
-            String buyType = buyCustomers.get(i).get_wanted_type();
-//            //Check inv map for type of item
-            ArrayList<Item> potentialItems = inv.get_items_of_type(buyType);
-//            //If none exist, customer leaves
-            if (potentialItems.size() == 0) {
-                System.out.println(buyCustomers.get(i).get_name() + " wanted to buy a " + buyType + " but none were in inventory, so they left.");
-            }
-            //If exist:
-            else {
-                // Just get the first item of the type. Could change this later
-                Item toBuyItem = potentialItems.get(0);
-
-                //50% chance to pay full price
-                Boolean buyAtFiftyPercent = buyCustomers.get(i).haggle_roll(50);
-
-                if (buyAtFiftyPercent) {
-                    // Remove the bought item from inventory, set daySold and salePrice, update cash register with sale price, move item to soldItems, remove the customer
-                    toBuyItem.set_day_sold(get_store().get_calendar().get_current_day());
-                    toBuyItem.set_sale_price(toBuyItem.get_list_price());
-                    reg.set_amount(reg.get_amount() + toBuyItem.get_sale_price());
-
-                    System.out.println(get_name() + " sold a " + buyType + " to " + buyCustomers.get(i).get_name() + " for $" + toBuyItem.get_sale_price());
-
-                    get_store().remove_from_inventory(toBuyItem);
-                    get_store().add_to_sold(toBuyItem);
-                }
-                //If fails, offer 10% discount
-                else {
-                    toBuyItem.set_list_price(0.90 * toBuyItem.get_list_price());
-
-                    //75% chance to accept
-                    Boolean buyAtSeventyFivePercent = buyCustomers.get(i).haggle_roll(75);
-                    if (buyAtSeventyFivePercent) {
-                        // Remove the bought item from inventory, set daySold and salePrice, update cash register with sale price, move item to soldItems, remove the customer
-                        toBuyItem.set_day_sold(get_store().get_calendar().get_current_day());
-                        toBuyItem.set_sale_price(toBuyItem.get_list_price());
-                        reg.set_amount(reg.get_amount() + toBuyItem.get_sale_price());
-
-                        System.out.println(get_name() + " sold a " + buyType + " to " + buyCustomers.get(i).get_name() + " for $" + toBuyItem.get_sale_price() + " after a 10% discount.");
-
-                        get_store().remove_from_inventory(toBuyItem);
-                        get_store().add_to_sold(toBuyItem);
-                    }
-                    else {
-                        System.out.println(get_name() + " tried selling a " + toBuyItem.get_condition().get_condition() + " condition " + toBuyItem.get_new_or_used() + " " + toBuyItem.get_name() + " to " + buyCustomers.get(i).get_name() + " for $" + toBuyItem.get_list_price() + " but customer refused.");
-                    }
-                }
-            }
-        }
 
         //Selling customer:
         for (int i = 0; i < sellCustomers.size(); i++)
