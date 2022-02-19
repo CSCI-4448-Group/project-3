f607af1105bd5e77b44ee391c5b376cfde5218db
brno4758 <brno4758@colorado.edu>
2022-02-18 21:00:27 -0700

    Finished tune behaviors

--- src/HaphazardTune.java
+++ src/HaphazardTune.java
@@ -6,13 +6,13 @@ public class HaphazardTune implements TuneBehavior{
     }
 
     public void tuneWind(Wind item){
-        if(rand_.nextInt(100)<50){
+        if(rand_.nextInt(100) < 50){
             item.set_adjusted(!item.get_adjusted());
         }
     }
 
     public void tunePlayers(Players item){
-        if(rand_.nextInt(100)<50){
+        if(rand_.nextInt(100) < 50){
             item.set_equalized(!item.get_equalized());
         }
     }
